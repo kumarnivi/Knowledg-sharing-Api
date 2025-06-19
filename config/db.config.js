fb638e3aa97ad90390162a7b6a57a696469106c7
kumarnivi <nivethikashivakumar56@gmail.com>
2025-06-19 21:52:07 +0530

    remove from git (.env and Nodemodules)

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 
-module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-  dialect: "mysql",
-};
+export const HOST = process.env.DB_HOST;
+export const USER = process.env.DB_USER;
+export const PASSWORD = process.env.DB_PASSWORD;
+export const DB = process.env.DB_NAME;
+export const dialect = "mysql";
 
 
